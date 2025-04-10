2ce67e6e70517ce0b5f07088211660c918dd503a
GundapuDivyasri <divyasrigundapu@gmail.com>
2025-04-10 10:19:53 +0530

    new url updated

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ document.getElementById("signupForm")?.addEventListener("submit", async function
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("http://localhost:5000/api/auth/signup", {
+  const res = await fetch("https://musicplaylist-backend.onrender.com/api/auth/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -27,7 +27,7 @@ document.getElementById("loginForm")?.addEventListener("submit", async function 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("http://localhost:5000/api/auth/login", {
+  const res = await fetch("https://musicplaylist-backend.onrender.com/api/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
