75f79bacc76cb76a47c562f51a14e4a28cfb1953
GundapuDivyasri <divyasrigundapu@gmail.com>
2025-04-16 14:30:18 +0530

    final updation

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..')));
 
 app.use('/songs', express.static(path.join(__dirname,'../songs')));
 app.use('/api/auth', authRoutes);
